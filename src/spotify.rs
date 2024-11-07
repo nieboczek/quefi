2baa23ea6c887d50e036d0c18fb78fac19a32c41
nieboczek <bartekkoraliki@gmail.com>
2024-11-07 17:35:07 +0100

    separate App and main

--- src/spotify.rs
+++ src/spotify.rs
@@ -1,3 +1,13 @@
-/// Spotify download module
-/// Searches YouTube for matches with song, and downloads it
-/// TODO: actually make it :)
+use crate::Config;
+use rspotify::{clients::BaseClient, model::PlaylistId, ClientCredsSpotify, Credentials};
+
+fn init_client(config: &Config) -> ClientCredsSpotify {
+    ClientCredsSpotify::new(Credentials::new(
+        &config.spotify_client_id,
+        &config.spotify_client_secret,
+    ))
+}
+
+fn get_playlist(client: ClientCredsSpotify, id: String) {
+    client.playlist_items(PlaylistId::from_id(id).unwrap(), None, None);
+}
